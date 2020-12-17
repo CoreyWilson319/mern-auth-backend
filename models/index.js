a424cf7cf2b78c2d04ed9c0106c3f0d3204f2280
Your Name <you@example.com>
2020-12-17 14:35:05 -0500

    [User] Add model and export model

--- models/index.js
+++ models/index.js
@@ -1,12 +1,15 @@
+require("dotenv").config();
+const keys = require('../config/keys')
 const mongoose = require("mongoose");
 
 // Mongo connection
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(keys.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: true,
 });
 
+console.log(process.env.MONGOURI)
 // Mongoose connection object
 const db = mongoose.connection;
 
@@ -19,6 +22,7 @@ db.once('open', () => {
 db.on('error', (error) => {
     console.log(`Datatbase error \n ${error}`)
 });
+
 
 // Example of another way to export
 // const User = require('./User');
