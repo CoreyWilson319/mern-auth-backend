16609fb6a44cd064d1f01e9264e8f208e927951f
Your Name <you@example.com>
2020-12-18 10:31:21 -0500

    [users] Add /login route and test

--- api/users.js
+++ api/users.js
@@ -1,48 +1,91 @@
 // Imports
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const passport = require('passport')
-const db = require('../models')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const db = require("../models");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Models
-const User = require('../models/User')
+const User = require("../models/User");
 
 // GET api/users/test (Public)
-router.get('/test', (req, res) => {
-    res.json({ msg: 'User endpoint OK!'});
+router.get("/test", (req, res) => {
+  res.json({ msg: "User endpoint OK!" });
 });
 
 // POST api/users/register (Public)
-router.post('/register', (req, res) => {
-    // Find user by email
-    User.findOne({ email: req.body.email })
-    .then(user => {
-        // if email already exist, send a 400 response
-        if (user) {
-            return res.status(400).json({ msg: 'Email already exists' })
-        } else {
-            // Create a new user
-            const newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password
-            });
-            // Salt and hash the password, then save the user
-            bcrypt.genSalt(10, (err, salt) => {
-                if (err) throw Error;
-                bcrypt.hash(newUser.password, salt, (error, hash) => {
-                    if (error) throw Error;
-                    // Change the password in newUser to the hash
-                    newUser.password = hash;
-                    newUser.save()
-                    .then(createdUser => res.json(createdUser))
-                    .catch(err2 => console.log(err2))
-                })
-            })
-        }
-    })
-})
+router.post("/register", (req, res) => {
+  // Find user by email
+  User.findOne({ email: req.body.email }).then((user) => {
+    // if email already exist, send a 400 response
+    if (user) {
+      return res.status(400).json({ msg: "Email already exists" });
+    } else {
+      // Create a new user
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      // Salt and hash the password, then save the user
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) throw Error;
+        bcrypt.hash(newUser.password, salt, (error, hash) => {
+          if (error) throw Error;
+          // Change the password in newUser to the hash
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((createdUser) => res.json(createdUser))
+            .catch((err2) => console.log(err2));
+        });
+      });
+    }
+  });
+});
+
+// POST api/users/login (Public)
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find a user via the email
+  db.User.findOne({ email: email})
+  .then(user => {
+      // If there is not a user
+      console.log(user)
+      if (!user) {
+          res.status(400).json({ msg: 'User not found'});
+      } else {
+          // A user is found in the database
+          bcrypt.compare(password, user.password)
+          .then(isMatch => {
+              // Check password for a match
+              if (isMatch) {
+                  console.log(isMatch)
+                  // User match, send a JSON Web Token
+                  // Create a token payload
+                  const payload = {
+                      id: user.id,
+                      email: user.email,
+                      name: user.name
+                  };
+                  // Sign token
+                  // 3600000 is one hour
+                  jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' }, (error, token) => {
+                      res.json({
+                          success: true,
+                          token: `Bearer ${token}`
+                      });
+                  });
+              } else {
+                  return res.status(400).json({ msg: 'Email or Password is incorrect' });
+              }
+          })
+      }
+  })
+});
 module.exports = router;
